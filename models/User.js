6fcc487658d929399a714d08214d699d506f940a
Yashnanhe04 <127133922+Yashnanhe04@users.noreply.github.com>
2024-05-14 02:48:52 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+  }
+}, {
+  timestamps: true
+});
+
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
